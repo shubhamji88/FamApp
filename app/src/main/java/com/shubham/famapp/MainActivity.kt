bbd3f5c8f4fd4e1af6d8c8d3fc3a98377c81720d
shubhamji88 <shubham.shankaram2019@vitstudent.ac.in>
2022-07-05 16:47:14 +0530

    initial commit

---
+++ app/src/main/java/com/shubham/famapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shubham.famapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
